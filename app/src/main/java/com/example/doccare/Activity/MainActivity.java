586684b8f6010b49a9c482b214779d8af538d835
Vu <nguyenbavu755@gmail.com>
2022-03-13 16:31:14 +0700

    test thu 1

--- app/src/main/java/com/example/doccare/Activity/MainActivity.java
+++ app/src/main/java/com/example/doccare/Activity/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello this commit uk uk uk dang thu");
+        System.out.println("test thu");
     }
 }
